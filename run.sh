2ff9cff5be3f18a82acf45ed6b3493a7383f6079
Eugene Cheung <ey.cheung@outlook.com>
2017-02-26 12:04:48 -0500

    Only add courses charts with run script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python ./generate_charts.py ../uoft-timetable-archive/201701/
 
-git add .
-git commit -a -m "Update charts (`date +'%Y-%m-%d %H:%M:%S'`)"
+git add courses/*
+git commit -m "Update charts (`date +'%Y-%m-%d %H:%M:%S'`)"
 git push origin master
