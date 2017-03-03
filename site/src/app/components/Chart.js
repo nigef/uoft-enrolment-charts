f492ae60eba54ed3383769fc4a24bbcaa9540429
Eugene Cheung <ey.cheung@outlook.com>
2017-03-02 19:31:37 -0500

    Revert site to basic one

--- site/src/app/components/Chart.js
+++
@@ -1,8 +0,0 @@
-import Inferno from 'inferno';
-import Component from 'inferno-component';
-
-const RAW_URL = 'https://cdn.rawgit.com/arkon/uoft-enrolment-charts/master';
-
-export default function Chart({ dept, course }) {
-  return <img src={`${RAW_URL}/${dept}/${coures}`} />;
-}
