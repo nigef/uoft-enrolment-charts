6ef8dcc75e6194ffa1269fa63cb6c264c485e9e3
Eugene Cheung <ey.cheung@outlook.com>
2017-03-04 17:14:56 -0500

    Fix script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python ./generate_charts.py ../uoft-timetable-archive/201701/
 
-git add courses/*
+git add charts/*
 git commit -m "Update charts (`date +'%Y-%m-%d %H:%M:%S'`)"
 git push origin master
