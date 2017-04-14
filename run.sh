333dfc9a83c29835ed2e2e6cb8cae4674f9c5bdd
Eugene Cheung <ey.cheung@outlook.com>
2017-04-14 11:38:32 -0400

    Generate listing on site instead of fetching on load

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+# Update charts
 python ./generate_charts.py ../uoft-timetable-archive/201701/
 
 git add charts/*
 git commit -m "Update charts (`date +'%Y-%m-%d %H:%M:%S'`)"
 git push origin master
+
+# Update site
+git show -s --format=%H > site/data/commit.yml
+tree -J --noreport -o charts.json ./charts
+mv charts.json site/data/charts.json
+cd site
+bundle exec middleman deploy
--- site/source/javascripts/site.js
+++ site/source/javascripts/site.js
@@ -1,69 +1,10 @@
 'use strict';
 
-(function() {
+$(document).on('click', '.course', function(e) {
+  var courseSvg = e.currentTarget.dataset.chart;
 
-var RAW_URL = 'https://cdn.rawgit.com/arkon/uoft-enrolment-charts/';
-
-var getRepoTree = function(cb) {
-  $.getJSON('https://api.github.com/repos/arkon/uoft-enrolment-charts/git/trees/master?recursive=1', cb);
-};
-
-var getDeptFromPath = function(path) {
-  return path.substring(path.indexOf('/') + 1);
-};
-
-var getCourseFromPath = function(path) {
-  var fileDir = path.lastIndexOf('/') + 1;
-  var ext = path.indexOf('.svg');
-
-  return path.substring(fileDir, ext);
-};
-
-var getCharts = function(data) {
-  var charts = data.tree.filter(function(item) { return item.path.indexOf('charts/') === 0; });
-
-  var $elCourses = $('#courses');
-  $elCourses.html('');
-
-  var $elDept, $elCourse;
-
-  for (var i = 0; i < charts.length; i++) {
-    var chart = charts[i];
-
-    if (chart.type === 'tree') {
-      $elDept = $('<details><summary>' + getDeptFromPath(chart.path) + '</summary></details>');
-      $elCourses.append($elDept);
-    } else if (chart.type === 'blob') {
-      $elCourse = $('<li>').text(getCourseFromPath(chart.path));
-      $elCourse.addClass('course');
-      $elCourse.attr('data-chart', data.sha + '/' + chart.path);
-
-      $elDept.append($elCourse);
-    }
-  }
-};
-
-var createIMG = function(url) {
   var elImg = document.createElement('IMG');
-  elImg.src = url;
+  elImg.src = courseSvg;
 
-  return elImg;
-};
-
-var registerClickEvents = function() {
-  $(document).on('click', '.course', function(e) {
-    var courseSvg = e.currentTarget.dataset.chart;
-    var elImg = createIMG(RAW_URL + courseSvg);
-
-    $('#chart').html(elImg);
-  });
-};
-
-var main = function() {
-  getRepoTree(getCharts);
-  registerClickEvents();
-};
-
-main();
-
-})();
+  $('#chart').html(elImg);
+});
