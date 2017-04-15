178c53dca268f53647d3b5d5b8230a7773661433
Eugene Cheung <ey.cheung@outlook.com>
2017-04-15 12:34:41 -0400

    Minify HTML

--- site/config.rb
+++ site/config.rb
@@ -1,7 +1,5 @@
-page '/*.json', layout: false
-
-# Build-specific configuration
 configure :build do
+  activate :minify_html, remove_quotes: false
   activate :minify_css
   activate :minify_javascript
   activate :asset_host, :host => '/uoft-enrolment-charts'
